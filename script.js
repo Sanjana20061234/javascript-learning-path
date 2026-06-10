3e3530f9b3fec7fabe51ab11e5e44d42bde70f5d
Sanjana Shah <sanjanashah6204ss@email.com>
2026-06-10 22:30:53 +0530

    Day 0: Initialized workspace and separated HTML/JS files

---
+++ script.js
@@ -0,0 +1,8 @@
+ document.getElementById("click-me").addEventListener("click", function() {
+            alert("Button clicked!");
+        });
+
+        document.querySelector(".my-button").addEventListener("click", function() {
+            alert("My button clicked!");
+        });
+        console.log("Buttons have been set up.");
