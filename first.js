e2908f464482d9483faf7adaa3b0bc7a73b74966
Sanjana Shah <sanjanashah6204ss@gmail.com>
2026-08-24 22:49:40 +0530

    Day 1: JavaScript Variables and Data Types

--- first.js
+++ first.js
@@ -1,1 +1,13 @@
-console.log("Sanjana Shah");
+Fullname = "Tony Stark";
+price = 1000;
+age = 24;
+x = null;
+y = undefined;
+isFollow = true;
+Fullname = 25;
+console.log(Fullname);
+console.log(price);
+console.log(age);
+console.log(x);
+console.log(y);
+console.log(isFollow);
