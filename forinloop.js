bbeee49e94ec2cfb787ad98ad09b3a09b1b1b4e8
Sanjana Shah <sanjanashah6204ss@gmail.com>
2026-09-02 18:57:40 +0530

    Added JavaScript loops and practice

---
+++ forinloop.js
@@ -0,0 +1,10 @@
+let student = {
+    name: "Sanjana Shah",
+    age: 20,
+    course: "Btech",
+    isGraduated: false,
+};
+for(let key in student){
+    console.log("key=", key, "value=", student[key]);
+    
+}
